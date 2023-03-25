7663f53a5d21fd6f890e05e47aa216e83067c402
claire-crg <82127458+claire-crg@users.noreply.github.com>
2023-03-25 16:50:52 -0400

    Add files via upload

--- compactness.py
+++ compactness.py
@@ -8,7 +8,7 @@ import sys
 import geopandas as gpd
 import pandas as pd
 import statistics as s
-
+import tkinter as tk
 
 #set virtual directory
 os.chdir("C:/Users/tup48123/Documents/ApplicationDevelopment/Project")
@@ -16,48 +16,49 @@ os.chdir("C:/Users/tup48123/Documents/ApplicationDevelopment/Project")
 # Importing module from github
 sys.path.insert(1, "C:/Users/tup48123/Documents/ApplicationDevelopment/Project")
 import compactness_measures as cm #only use base name of file
-import connect_census as cen
 
 ##
 
-##first on the to do list: where are we storing the outputs of each script and how are we pulling them into the next script?
 #pull user input from virtual directory
 #write files to a temp folder?
 
 #read in user inputted file
 shp = gpd.read_file("data/SHP/pa_vtd_2020_bound.shp", crs="4269")
 
-
-#second on the to do list:
-##ask user to specify as parameter which measure they want
-##make function able to accept a list of files
+user_input = ["Polsby Popper","Schwartzberg"]
 
 
 #########OUTPUT
 ## minimum or average or both compactness scores
 ## minimum because targetting in specific districts
 
+##make function able to accept a list of files
 
-def userDefMeasure(user_input, fn):
+#ask user to specify as parameter which measure they want
+
+
+def user_def_measure(user_input, fn):
     
-    ##since this script is coming in after the census pull, we don't need to
-    ##differentiate whether the input is text or shapefile
+    #user can pass multiple measures so more than 1 can be calculated
+    #pass in a list of measures
 
-    if user_input == "Polsby Popper":
+    if "Polsby Popper" in user_input:
         pp = cm.polsby_popper(fn)
         min_pp = ["min_pp", min(pp)]
         mean_pp = ["mean_pp", s.mean(pp)]
         
-        return min_pp, mean_pp
+        #return pd.Series([min_pp, mean_pp])
     
-    elif user_input == "Schwartzberg":
+    if "Schwartzberg" in user_input:
         schwb= cm.schwartzberg(fn)
-        min_schwb = min(schwb)
-        mean_schwb = s.mean(schwb)
+        min_schwb = ["min_schwb", min(schwb)]
+        mean_schwb = ["mean_schwb", s.mean(schwb)]
         
-        return min_schwb, mean_schwb
+        #return pd.Series([min_schwb, mean_schwb])
     
-    elif user_input == "Convex Hull Ratio":
+    return pd.Series([min_pp, mean_pp]), pd.Series([min_schwb, mean_schwb])
+    
+    if "Convex Hull Ratio" in user_input:
         hull= cm.c_hull_ratio(fn)
         min_hull = min(hull)
         mean_hull = s.mean(hull)
@@ -68,20 +69,21 @@ def userDefMeasure(user_input, fn):
 
 def compact(user_input, fn):
     
-    y = userDefMeasure(user_input, fn) #returns tuple with 2 lists
+    y = user_def_measure(user_input, fn) #returns tuple with 2 lists
     
-    data = {"planID": [1], #when more plans are added, column will have to populate ints for # of plans
-            f"{y[0][0]}": [y[0][1]],
-            f"{y[1][0]}": [y[1][1]]
-            }
-    
-    df = pd.DataFrame(data)
-    
-    return df
-
-
-
-#pass user file through compact()
-compact("Polsby Popper", shp)
-
+    for n in range(len(user_input)):
         
+        if len(user_input) < 2:
+            cols= {f"{y[0][0]}": y[0][1],
+                  f"{y[1][0]}": y[1][1]}
+            #print(cols)
+            
+        if len(user_input) > 1:
+            cols= {f"{y[n][0][0]}": y[n][0][1],
+                  f"{y[n][1][0]}": y[n][1][1]}
+            #print(cols)
+        
+    return cols
+    
+    
+compact(user_input, shp)
